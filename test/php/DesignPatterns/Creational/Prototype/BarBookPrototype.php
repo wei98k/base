142ffaaf02c622ae57a73e8a128dab283fe00065
mjw <majianwei168@outlook.com>
2020-12-16 10:55:47 +0800

    原型模式

---
+++ test/php/DesignPatterns/Creational/Prototype/BarBookPrototype.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+
+namespace DesignPatterns\Creational\Prototype;
+
+
+class BarBookPrototype extends BookPrototype
+{
+    protected string $category = 'Bar';
+
+    public function __clone()
+    {
+
+    }
+}
