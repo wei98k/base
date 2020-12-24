75d9d850e60a6c2272cc49143cdd36f7532bdc6b
mjw <majianwei168@outlook.com>
2020-12-24 10:04:43 +0800

    装饰器模式

---
+++ test/php/DesignPatterns/Structural/Decorator/BookingDecorator.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+
+namespace DesignPatterns\Structural\Decorator;
+
+
+abstract class BookingDecorator implements Booking
+{
+    protected Booking $booking;
+
+    public function __construct(Booking $booking)
+    {
+        $this->booking = $booking;
+    }
+}
