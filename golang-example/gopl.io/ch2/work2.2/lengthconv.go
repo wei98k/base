4b04fb97fe6128de723ed22362af9f77810140ca
peter <majianwei168@outlook.com>
2021-11-09 07:34:20 +0800

    gopl

---
+++ golang-example/gopl.io/ch2/work2.2/lengthconv.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+type Meter float64
+type Foot float64
+
+func (m Meter) String() string { return fmt.Sprintf("%gM", m) }
+func (f Foot) String() string  { return fmt.Sprintf("%gft", f) }
+
+func FToM(f Foot) Meter { return Meter(f / 3.2808) }
+func MToF(m Meter) Foot { return Foot(m * 3.2808) }
