6b2e1220445c3dd392a2ad5ddfdaa0e1e9c70a8f
peter <majianwei168@outlook.com>
2021-11-08 15:21:21 +0800

    gopl

---
+++ golang-example/gopl.io/ch1/server1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+//写个web服务
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
