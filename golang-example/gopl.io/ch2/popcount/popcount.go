003506dcf3e5135f6c60a5f8d035e64f94e061ab
peter <majianwei168@outlook.com>
2021-11-09 07:43:03 +0800

    gopl

---
+++ golang-example/gopl.io/ch2/popcount/popcount.go
@@ -0,0 +1,22 @@
+package popcount
+
+// 下面的代码定义了一个PopCount函数，用于返回一个数字中含二进制1bit的个数。
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
---
+++ golang-example/gopl.io/ch2/popcount/popcount_test.go
@@ -0,0 +1,67 @@
+package popcount
+
+import "testing"
+
+func BitCount(x uint64) int {
+	// Hacker's Delight, Figure 5-2
+	x = x - ((x >> 1) & 0x5555555555555555)
+	x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+	x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
+	x = x + (x >> 8)
+	x = x + (x >> 16)
+	x = x + (x >> 32)
+	return int(x & 0x7f)
+}
+
+func PopCountByClearing(x uint64) int {
+	n := 0
+	for x != 0 {
+		x = x & (x - 1) // clear rightmost non-zero bit
+		n++
+	}
+	return n
+}
+
+func PopCountByShifting(x uint64) int {
+	n := 0
+	for i := uint(0); i < 64; i++ {
+		if x&(1<<i) != 0 {
+			n++
+		}
+	}
+	return n
+}
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkBitCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		BitCount(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountByClearing(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountByClearing(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountByShifting(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountByShifting(0x1234567890ABCDEF)
+	}
+}
+
+// jianwei% go test -bench=.
+// goos: darwin
+// goarch: amd64
+// pkg: example/gopl.io/ch2/popcount
+// cpu: Intel(R) Core(TM) i5-5350U CPU @ 1.80GHz
+// BenchmarkPopCount-4                     1000000000            0.3582 ns/op
+// BenchmarkBitCount-4                     1000000000            0.3554 ns/op
+// BenchmarkPopCountByClearing-4           64660414             16.49 ns/op
+// BenchmarkPopCountByShifting-4           38527489             29.04 ns/op
