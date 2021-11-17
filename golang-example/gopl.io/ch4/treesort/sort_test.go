53b9a1b2d7a58875b037dc979aa53c62d0b35269
peter <majianwei168@outlook.com>
2021-11-17 08:37:38 +0800

    gopl

---
+++ golang-example/gopl.io/ch4/treesort/sort_test.go
@@ -0,0 +1,16 @@
+package treesort
+
+import (
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	// data := make([]int, 50)
+	// for i := range data {
+	// 	data[i] = rand.Int() % 50
+	// }
+	// Sort(data)
+	// if !sort.IntsAreSorted(data) {
+	// 	t.Errorf("not sorted: %v", data)
+	// }
+}
