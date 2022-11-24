5dee80ee9c9741ca0304e88ee6ee9c4264e3f206
jacky <majianwei168@outlook.com>
2022-11-24 09:21:30 +0800

    插入排序

--- js/algo/sort/insert.js
+++ js/algo/sort/insert.js
@@ -1,22 +1,44 @@
 
 let arr = [3,2,9,2,4,8,7,5,6]
 
-let res = insertionSort(arr)
-// console.log(res)
+let res = inserSort(arr)
+//let res = insertionSort(arr)
+console.log(res)
 
-// 插入排序
-function insertionSort(arr) {
-    var len = arr.length;
-    var preIndex, current;
-    for (var i = 1; i < len; i++) {
-        preIndex = i - 1;
-        current = arr[i];
+// function inserSort(arr) {
+//     // 循环全部需要对比的元素
+//     // N+1比较前面X的全部元素
+//     // N+1位置需要比N位置的元素大就在X+1位置插入
+//     // 循环上面操作找到结束
+//     // Tip: 0等于temp  循环1开始
+//     for(let i = 1; i<arr.length; i++) {
+//         let j = i - 1;
+//         let temp = arr[i]
+//         for(j;j >=0; j--) {
+//             // 比较之前已经比较过的元素
+//             // 依次比较，比元素小，之前的元素就后移位置
+//             if(temp < arr[j]) {
+//                 arr[j+1] = arr[j]
+//             } else {
+//                 break;
+//             }
+//         }
+//         // 当之前的元素没有比当前元素小的了，就在这个位置插入当前的元素
+//         arr[j+1] = temp
+//     }
+//     return arr
+// }
 
-        while(preIndex >= 0 && arr[preIndex] > current) {
-            arr[preIndex+1] = arr[preIndex];
-            preIndex--;
+function inserSort(arr) {
+    for(let i = 1; i < arr.length; i++) {
+        let temp = arr[i];
+        let j = i - 1
+        while(j >= 0 && temp < arr[j]) {
+            arr[j+1] = arr[j]
+            --j
         }
-        arr[preIndex+1] = current;
+        // console.log(temp)
+        arr[j+1] = temp
     }
-    return arr;
+    return arr
 }
