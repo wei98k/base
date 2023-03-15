efbfc4f9a3ef3910c420c2fab4dad4ad9bbd8579
wei <majianwei168@outlook.com>
2023-03-15 17:34:13 +0800

    优化

--- python/scrapy/spider2107/spider2107/spiders/douban.py
+++ python/scrapy/spider2107/spider2107/spiders/douban.py
@@ -11,8 +11,9 @@ class DoubanSpider(scrapy.Spider):
     def parse(self, response):
         sel = Selector(response)
         list_items = sel.css('#content > div > div.article > ol > li')
-        movie_item = MovieItem()
+
         for list_item in list_items:
+            movie_item = MovieItem()
             movie_item['title'] = list_item.css('span.title::text').extract_first()
             movie_item['rank'] = list_item.css('span.rating_num::text').extract_first()
             list_item.css('span.inq::text').extract_first()
