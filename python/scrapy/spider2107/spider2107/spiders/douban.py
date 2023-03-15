7359a66414d544b2afdc4365bced2a18790d4c2d
wei <majianwei168@outlook.com>
2023-03-15 11:19:51 +0800

    douban top250 首页数据

---
+++ python/scrapy/spider2107/spider2107/spiders/douban.py
@@ -0,0 +1,19 @@
+import scrapy
+from scrapy import Selector
+
+from spider2107.items import MovieItem
+
+class DoubanSpider(scrapy.Spider):
+    name = "douban"
+    allowed_domains = ["movie.douban.com"]
+    start_urls = ["https://movie.douban.com/top250"]
+
+    def parse(self, response):
+        sel = Selector(response)
+        list_items = sel.css('#content > div > div.article > ol > li')
+        movie_item = MovieItem()
+        for list_item in list_items:
+            movie_item['title'] = list_item.css('span.title::text').extract_first()
+            movie_item['rank'] = list_item.css('span.rating_num::text').extract_first()
+            list_item.css('span.inq::text').extract_first()
+            yield movie_item
